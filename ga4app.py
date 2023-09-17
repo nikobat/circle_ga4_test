1eb0c978d05d12b6eb5cc4a088d228d0d4121c7f
nikobat <45957251+nikobat@users.noreply.github.com>
2023-09-17 17:01:11 +1200

    3

--- ga4app.py
+++ ga4app.py
@@ -1,17 +1,55 @@
 import streamlit as st
 import pandas as pd
 import os
+import json
+import tempfile
 from google.cloud import bigquery
+from io import StringIO
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'teamcircle-399006-af8e226fd4ff.json'
+# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'teamcircle-399006-af8e226fd4ff.json'
 
-# st.set_page_config(layout="wide", page_icon="🛠️", page_title="GA4 RULZ")
-# st.title("Getting stuff out of GA4")
-# st.subheader("This will make it easy and useful")
+st.set_page_config(layout="wide", page_icon="🛠️", page_title="GA4 RULZ")
+st.title("Getting stuff out of GA4")
+st.subheader("This will make it easy and useful")
 
-# st.sidebar.text_input("Something for JSON")
+if  os.path.isfile('teamcircle-399006-af8e226fd4ff.json') == True:
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'teamcircle-399006-af8e226fd4ff.json'
+else:
+    json_file = st.file_uploader("Drop your JSON here", type="json")
+if not json_file:
+    st.info("Upload JSON Authenticaor to continue")
+    st.stop()
 
-client = bigquery.Client()
+# Get IDs for Project before continuing
+if "PROJECT_ID" in st.secrets:
+    project_id = st.secrets["PROJECT_ID"]
+else:
+    project_id = st.sidebar.text_input("Project ID")
+if not project_id:
+    st.info("Enter a Project ID to continue")
+    st.stop()
+
+if "DATASET_ID" in st.secrets:
+    dataset_id = st.secrets["DATASET_ID"]
+else:
+    dataset_id = st.sidebar.text_input("Dataset ID")
+if not dataset_id:
+    st.info("Enter a Dataset ID to continue")
+    st.stop()
+
+with tempfile.NamedTemporaryFile(delete=False) as fp:
+    fp.write(json_file.getvalue())
+try:
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = fp.name          
+    with open(fp.name,'rb') as a:
+  
+         client = bigquery.Client()
+finally:
+          if os.path.isfile(fp.name):
+              os.unlink(fp.name)
+
+
+
 
 def get_unique_keys_and_types(client, project_id, dataset_id, table_patterns):
     print("Getting unique keys and their types...")
@@ -31,7 +69,12 @@ def get_unique_keys_and_types(client, project_id, dataset_id, table_patterns):
     query = " UNION ALL ".join(union_subqueries) + " GROUP BY key, value_type"
     query_job = client.query(query)
     keys_and_types = query_job.result()
-    print("Unique keys and types retrieved successfully.")
+    st.write("Unique keys and types retrieved successfully.")
     return {row.key: row.value_type for row in keys_and_types}
 
-get_unique_keys_and_types
+# Define the table patterns to query
+table_patterns = ["events_2023*", "events_intraday_2023*"]
+user_table_pattern = "pseudonymous_users_2023*"
+
+# Get the unique keys and their types
+keys_and_types = get_unique_keys_and_types(client, project_id, dataset_id, table_patterns)
