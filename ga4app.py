1041b83aa351848842b6a8607e4d6eb69a5a48ae
nikobat <45957251+nikobat@users.noreply.github.com>
2023-09-17 16:02:21 +1200

    3

--- ga4app.py
+++ ga4app.py
@@ -1,8 +1,37 @@
 import streamlit as st
 import pandas as pd
+import os
+from google.cloud import bigquery
 
-st.set_page_config(layout="wide", page_icon="🛠️", page_title="GA4 RULZ")
-st.title("Getting stuff out of GA4")
-st.subheader("This will make it easy and useful")
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'teamcircle-399006-af8e226fd4ff.json'
 
-st.sidebar.text_input("Something for JSON")
+# st.set_page_config(layout="wide", page_icon="🛠️", page_title="GA4 RULZ")
+# st.title("Getting stuff out of GA4")
+# st.subheader("This will make it easy and useful")
+
+# st.sidebar.text_input("Something for JSON")
+
+client = bigquery.Client()
+
+def get_unique_keys_and_types(client, project_id, dataset_id, table_patterns):
+    print("Getting unique keys and their types...")
+    union_subqueries = [
+        f"""
+        SELECT key, 
+               IF(ep.value.string_value IS NOT NULL, 'string', 
+                  IF(ep.value.int_value IS NOT NULL, 'int', 
+                     IF(ep.value.float_value IS NOT NULL, 'float', NULL)
+                  )
+               ) AS value_type
+        FROM `{project_id}.{dataset_id}.{table_pattern}`,
+        UNNEST(event_params) AS ep
+        """
+        for table_pattern in table_patterns
+    ]
+    query = " UNION ALL ".join(union_subqueries) + " GROUP BY key, value_type"
+    query_job = client.query(query)
+    keys_and_types = query_job.result()
+    print("Unique keys and types retrieved successfully.")
+    return {row.key: row.value_type for row in keys_and_types}
+
+get_unique_keys_and_types
