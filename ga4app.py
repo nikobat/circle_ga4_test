e9c0d11755be54d0be2596aac48a790467a86dd9
nikobat <45957251+nikobat@users.noreply.github.com>
2023-09-17 15:38:33 +1200

    Set up

---
+++ ga4app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(layout="wide", page_icon="🛠️", page_title="GA4 RULZ")
+st.title("Getting stuff out of GA4")
+st.subheader("This will make it easy and useful")
+
+st.sidebar.text_input("Something for JSON")
