d86fc90109a68650286f7dd90d8dab916c52eec9
nikobat <45957251+nikobat@users.noreply.github.com>
2023-10-08 16:58:48 +1300

    Instructions

--- ga4app.py
+++ ga4app.py
@@ -300,28 +300,60 @@ table_patterns = "events_*", "events_intraday_*"
 user_table_pattern = "pseudonymous_users_*"
 
 
-st.set_page_config(layout="wide", page_icon="🛠️", page_title="GA4 RULZ")
-st.title("Getting stuff out of GA4")
-st.subheader("This will make it easy and useful")
+st.set_page_config(layout="wide", page_icon=":unlock:", page_title="GA4 Data Transformer and Dashboard")
+st.title("Unlocking your GA4 data :unlock:")
 
 # Get IDs for Project before continuing
-
 
 tab1, tab2 = st.tabs(["Main", "Instructions"])
 
 #Explain what is the point of this tool
 with tab1:
-    st.write("### What am I?")
-    st.write("I am GA4 *cat* :cat: **MEOW**")
+    st.write('''
+             ## Explanation
+             ### For those curious about the technical aspects: 
+             This Streamlit app is designed to analyse data from Google Analytics 4 (GA4) stored in BigQuery. The steps above ensure that GA4 data is sent to BigQuery, and that the app can securely access this data. When you upload the JSON config file, you're providing the app with a set of credentials. These credentials act as a key, giving the app permission to read the GA4 data from BigQuery. It's a secure way to allow access without sharing sensitive information. Once the dataset is selected and the app runs, it processes the data using Python scripts, for non-technical users, think of this app as a bridge between your GA4 data in BigQuery and actionable insights. By providing the necessary setup and credentials, you're allowing the app to fetch, process, and visualize the data in a way that's meaningful and valuable to you.
+            ### Combining Daily and Intra-Daily Feeds for Both Event and User Data:
+            GA4 exports data to BigQuery in two primary feeds: daily and intra-daily, both for event data and user data. For event data, the daily feed gives a full snapshot of the previous day's events, while the intra-daily feed provides near real-time event data throughout the day. For user data, the daily feed captures a daily snapshot of user-level information, while the intra-daily feed provides incremental updates on user interactions. The code consolidates these feeds, ensuring that the combined data view is up-to-date, comprehensive, and consistent across both events and users.
+            1.	Assessing Data Types and Quality for Both Tables:  
+            Both the event and user tables in BigQuery can have various data types. The code assesses each column's data type in both tables to ensure consistency and accuracy. It evaluates data quality by pinpointing columns with significant missing data or inconsistent values, ensuring the integrity of both event and user data.
+            2.	Flattening Tables and Removing Nesting:  
+            Both GA4 event and user data in BigQuery are often structured in nested records. The code transforms these nested structures into flat tables, making them more accessible. For instance, nested event parameters in the event table and user properties in the user table are converted into individual columns.
+            3.	Dropping Unnecessary Columns:  
+            Not all columns in the GA4 datasets (both event and user tables) are relevant for every analysis. The code identifies and excludes columns with redundant or irrelevant data, streamlining both tables for easier and more efficient querying.
+            4.	Producing a Near Real-Time, Easily Reportable View for Both Tables:  
+            By integrating and cleaning both the event and user data, the code provides a unified and accessible view that's: Near Real-Time: Thanks to the incorporation of intra-daily feed data. Easily Reportable: The flat structure and optimized columns make querying straightforward. Contrast with GA4 UI: While the GA4 user interface is beneficial for general insights, the Streamlit app offers several advantages: Customization: The GA4 UI presents predefined reports. In contrast, accessing BigQuery data via the Streamlit app allows for tailored analyses specific to individual needs, leveraging both event and user-level data. Granularity: The Streamlit app provides a granular look into data, offering insights into specific event parameters or individual user behaviors, something the GA4 UI may not provide in-depth. Data Access: Users can interact directly with the raw data in BigQuery via the app, offering more flexibility in analyses than the GA4 UI, especially when correlating event and user data. In summary, the Streamlit app offers a deeper, more customizable dive into GA4 data by meticulously processing and presenting both event and user tables, ensuring comprehensive insights beyond what the standard GA4 UI might offer.
+            ''')
 
 # Step by step instructions and running
 with tab2:
+    st.write('''
+            ## Step-by-Step Guide
+            ### Connect Google Analytics 4 (GA4) to BigQuery:
+            **What this does:** This step ensures that your Google Analytics 4 data is being sent to BigQuery, making it accessible for further analysis.
+            1. Go to your GA4 Property.
+            2. Navigate to Integrations.
+            3. Find BigQuery in the list and click on Link.
+            4. Follow the on-screen instructions to complete the linking process.  
+             ''')
     if  os.path.isfile('teamcircle-399006-af8e226fd4ff.json') == True:
         json_file = os.path.isfile('teamcircle-399006-af8e226fd4ff.json')
         os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'teamcircle-399006-af8e226fd4ff.json'
         client = bigquery.Client()
     else:
-        st.write("Generate your JSON by balah blah")
+        st.write('''
+            ### Generate JSON Config File for GCP API Access:
+            **What this does:** This JSON file is a key that allows applications (like our Streamlit app) to access your BigQuery data on your behalf. It's essential to keep this file secure. By uploading the JSON, you're giving the Streamlit app the credentials it needs to access and process your BigQuery data.
+            1. Go to the Google Cloud Console
+            2. Select the project linked with your GA4.
+            3. Navigate to IAM & Admin > Service accounts.
+            4. Click on Create Service Account and give it a descriptive name.
+            5. Grant appropriate roles. For BigQuery, roles like BigQuery Data Viewer and BigQuery User are essential.
+            6. Click Continue and then Done.
+            7. Now, find the service account you just created in the list, click on the three-dot menu, and select Manage keys.
+            8. Click on Add Key and choose JSON. This will download a JSON file to your computer.
+            9. Upload the file below.  
+            ''')
         json_file = st.file_uploader("Drop your JSON here", type="json")
     if not json_file :
         st.info("Upload GA JSON Authenticator to continue")
@@ -341,7 +373,12 @@ with tab2:
     if "PROJECT_ID" in st.secrets:
         project_id = st.secrets["PROJECT_ID"]
     else:
-        st.write("Give us your project id from balah blah")
+        st.write('''
+            ### Select Project and Dataset:
+            **What this does:** The app will now use the provided credentials to access the selected project/dataset in BigQuery and perform the analysis. Once you have done this the project will run
+            1. Type in the project ID that you want the app to use
+            2. Type in the dataset ID that you want the app to use
+            ''')
         project_id = st.text_input("Project ID")
     if not project_id:
         st.info("Enter a Project ID to continue")
